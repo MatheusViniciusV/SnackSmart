b68d8e33407010f0e83018a1e6146993b0af3607
El0y-C0SM0 <eloymaciel06@gmail.com>
2023-10-19 00:41:09 -0300

    tá rodando mas falta corrigir as dependencias, trabalhando no form de criar contratos

--- code/View/src/main/webapp/js/contratos.js
+++ code/View/src/main/webapp/js/contratos.js
@@ -44,12 +44,10 @@ if($contratoSelecionado !== null) {
         };
     
         $.ajax({
-            type: 'POST', // Tipo de solicitação (POST)
-            url: 'SolicitarCancelarContrato', // URL do servlet
-            data: contratoDataSend, // Dados a serem enviados no corpo da solicitação
+            type: 'POST', 
+            url: 'SolicitarCancelarContrato', 
+            data: contratoDataSend,
             success: function (response) {
-                // Ação a ser executada em caso de sucesso
-                // alert("Solicitação bem-sucedida:" + response);
     
                 $('#solicita-cancelar-contrato').addClass('btn-confirmado');
     
@@ -82,6 +80,81 @@ $('#cancelar').click(function () {
 });
 
 $('#enviar').click(function() {
-    alert('ainda não implementado.');
-    $('#criar-contrato-form').addClass('oculto')
+    let formCorreto = true;
+    const cpfRegEx = /\d{3}\.\d{3}\.\d{3}\.-\d{2}/;
+    const telRegEx = /\(\d{2}\)\d{9}/
+    const emailRegEx = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+
+    function validarRegEx(input, regEx) {
+        let $input = $(input);
+
+        if(!regEx.test($input.val())) {
+            $input.addClass('errado');
+            $input.val('');
+            formCorreto = false;
+        }
+    } 
+
+    validarRegEx('input[name=locatario-cpf', cpfRegEx);
+    validarRegEx('input[name=locatario-email', emailRegEx);
+    validarRegEx('input[name=locatario-telefone', telRegEx);
+
+    $('input').each(function() {
+        if($(this).val() == '') {
+            $(this).addClass('errado');
+            formCorreto = false;
+        } else {
+            $(this).removeClass('errado');
+        }
+    });
+
+
+
+    if(formCorreto) {
+
+        alert('ainda não implementado.');
+        $('#criar-contrato-form').addClass('oculto');
+    
+        const dataContrato = {
+            // dados do locatario
+            nomeLocatario: $('input[name=locatario-nome').val(),
+            cpfLocatario: $('input[name=locatario-cpf').val(),
+            emailLocatario: $('input[name=locatario-email').val(),
+            telefoneLocatario: $('input[name=locatario-telefone').val(),
+            // dados do contrato
+            dataInicio: $('input[name=data-inicio').val(),
+            dataTermino: $('input[name=data-termino').val(),
+            dataPagamento: $('input[name=data-pagamento').val(),
+            Observacoes: $('textarea[name=observacoes]').val(),
+        }
+    
+        $.ajax({
+            type: 'POST', // Tipo de solicitação (POST)
+            url: 'CriarContrato', // URL do servlet
+            data: dataContrato, // Dados a serem enviados no corpo da solicitação
+            success: function (response) {
+                // Ação a ser executada em caso de sucesso
+                // alert("Solicitação bem-sucedida:" + response);
+    
+                $('#solicita-cancelar-contrato').addClass('btn-confirmado');
+    
+                setTimeout(function() {
+                    $contratoSelecionado.remove();
+                    $('#solicita-cancelar-contrato').removeClass('btn-confirmado');
+                    $('#solicita-cancelar-contrato').addClass('null');
+                    $('#pdf-contrato').addClass('null');
+                    $('#pdf-contrato a').removeAttr('href');
+            
+                }, 750);
+    
+                console.log(`Contrato ${contratoDataSend.id} deletado com sucesso.`);
+            },
+            error: function (error) {
+                
+                console.error(error);
+    
+            }
+        });
+    }
+
 });
