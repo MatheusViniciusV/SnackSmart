91a732fc6bebe15e618f465dc20f6a56738603da
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2023-10-31 02:41:59 -0300

    Update de LocatarioDTO

--- code/Core/src/main/java/br/cefetmg/snacksmart/dto/LocatarioDTO.java
+++ code/Core/src/main/java/br/cefetmg/snacksmart/dto/LocatarioDTO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package br.cefetmg.snacksmart.dto;
 
 /**
@@ -9,18 +5,83 @@ package br.cefetmg.snacksmart.dto;
  * @author eloym
  */
 public class LocatarioDTO {
-    private String nome; 
+    
+    private Long pk;
+    private String usuario;
+    private String nome;
+    private String senha;
+    private String cpf;
+    private String rg;
+    private String telefone;
+    private String email; 
+    
     public LocatarioDTO(String nome){
         this.nome = nome;
     }
-    public LocatarioDTO(){
-        
+    
+    public LocatarioDTO() {}
+    
+    public Long getPk() {
+        return pk;
     }
-    public String getCPF() {
-        return null;
+
+    public void setPk(Long pk) {
+        this.pk = pk;
     }
     
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
     public String getNome() {
         return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getCPF() {
+        return cpf;
+    }
+
+    public void setCPF(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    public String getRG() {
+        return rg;
+    }
+
+    public void setRG(String rg) {
+        this.rg = rg;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
