8f78a9101d5f7cbfa327fd186b7889fbcd8a2b1c
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2023-10-31 02:43:25 -0300

    Correção do MysqlConnection

--- code/Core/src/main/java/br/cefetmg/snacksmart/utils/bd/MysqlConnection.java
+++ code/Core/src/main/java/br/cefetmg/snacksmart/utils/bd/MysqlConnection.java
@@ -9,14 +9,14 @@ import java.sql.SQLException;
  * @author eloym
  */
 public class MysqlConnection implements ConnectionFactory {
-    private final static String dbDriver = "org.postgresql.Driver";
-    private final static String dbURL = "jdbc:mysql://localhost:3306/snacksmart?serverTimezone=UTC";
+    private final static String dbDriver = "com.mysql.cj.jdbc.Driver";
+    private final static String dbURL = "jdbc:mysql://localhost:3306/snacksmart";
     private final static String user = "root";
     private final static String pass = "";
     
     @Override
     public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
+        Class.forName(dbDriver);
         return DriverManager.getConnection(dbURL, user, pass);
     }
 }
