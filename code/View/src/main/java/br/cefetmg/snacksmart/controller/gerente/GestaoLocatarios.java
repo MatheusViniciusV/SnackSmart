975b397a5322d51ca52eb98d39639c22afa345bc
MatheusViniciusV <matheusviniciusvieiradias@gmail.com>
2023-11-19 14:47:41 -0300

    Corrigido problema ao atualizar senha vazia

--- code/View/src/main/java/br/cefetmg/snacksmart/controller/gerente/GestaoLocatarios.java
+++ code/View/src/main/java/br/cefetmg/snacksmart/controller/gerente/GestaoLocatarios.java
@@ -25,59 +25,39 @@ public class GestaoLocatarios extends HttpServlet {
         try (PrintWriter out = response.getWriter()) {
             response.setContentType("text/html;charset=UTF-8");
             
-            out.println("1");
-            
             LocatarioDAO locatarioDAO = new LocatarioDAO();
             ArrayList<LocatarioDTO> locatarios;
             LocatarioDTO locatario;
             
-            out.println("2");
-            
             String submitType = request.getParameter("submitType");
-            
-            out.println("3");
             
             String senha;
             MessageDigest algorithm;
             StringBuilder aux;
             byte hash[];
             
-            out.println("5");
-            
             if (null != submitType) switch (submitType) {
                 case "adicionar":
-                    
-                    out.println("6");
                     
                     locatario = new LocatarioDTO();
                     locatario.setNome(request.getParameter("nome"));
                     locatario.setCPF(request.getParameter("cpf"));
                     senha = request.getParameter("senha");
                     
-                    out.println("7");
-                    
                     algorithm = MessageDigest.getInstance("SHA-256");
                     hash = algorithm.digest(senha.getBytes("UTF-8"));
-                    
-                    out.println("8");
 
                     aux = new StringBuilder();
                     for (byte b : hash) {
                         aux.append(String.format("%02X", 0xFF & b));
                     }
-                    
-                    out.println("9");
 
                     senha = aux.toString();
-                    
-                    out.println("10");
                     
                     locatario.setSenha(senha);
                     locatario.setEmail(request.getParameter("email"));
                     locatario.setTelefone(request.getParameter("telefone"));
                     locatario.setRG(request.getParameter("rg"));
-                    
-                    out.println("11");
                     
                     locatarioDAO.inserir(locatario);
                     
@@ -89,23 +69,29 @@ public class GestaoLocatarios extends HttpServlet {
                     locatarioDAO.delete(locatario);
                     break;
                 case "atualizar":
+                    
+                    boolean senhavazia;
+                    
                     locatarios = locatarioDAO.listarTodos();
                     locatario = locatarios.get(Integer.parseInt(request.getParameter("lista")));
                     locatario.setNome(request.getParameter("nome"));
                     locatario.setCPF(request.getParameter("cpf"));
                     senha = request.getParameter("senha");
                     
-                    algorithm = MessageDigest.getInstance("SHA-256");
-                    hash = algorithm.digest(senha.getBytes("UTF-8"));
-
-                    aux = new StringBuilder();
-                    for (byte b : hash) {
-                        aux.append(String.format("%02X", 0xFF & b));
-                    }
-
-                    senha = aux.toString();
+                    if(senha != "") {
                     
-                    locatario.setSenha(senha);
+                        algorithm = MessageDigest.getInstance("SHA-256");
+                        hash = algorithm.digest(senha.getBytes("UTF-8"));
+
+                        aux = new StringBuilder();
+                        for (byte b : hash) {
+                            aux.append(String.format("%02X", 0xFF & b));
+                        }
+
+                        senha = aux.toString();
+
+                        locatario.setSenha(senha);
+                    }
                     locatario.setEmail(request.getParameter("email"));
                     locatario.setTelefone(request.getParameter("telefone"));
                     locatario.setRG(request.getParameter("rg"));
